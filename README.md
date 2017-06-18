d28f499b3afd707de000131097e545f4615ead92
TinyMan <raph.a.uzan@gmail.com>
2017-06-18 15:54:01 +0200

    Version 0.1.0
    * Support for remote audio MSE compatible WeBM files  (seekable)
    * Support for stream input or remote file (not seekable)
    * basic controls (play, pause, resume, stop, seek)
    * configurable playback options (transcoder settings)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# node-jamy
+# Jamy
 Jamy is a streaming Audio player for Node.Js aiming to support MSE compatible sources
---
+++ index.d.ts
@@ -0,0 +1,79 @@
+// Type definitions for Jamy 0.1.0
+// Project: https://github.com/TinyMan/node-jamy
+// Definitions by: TinyMan https://github.com/TinyMan
+
+export = Jamy;
+
+import * as EventEmitter from "events";
+import * as TimedStream from "timed-stream";
+import { Readable } from "stream";
+
+/**
+ * Jamy is an audio player designed to stream MSE compatible files when possible.
+ * Note that standart stream / non-mse compatible files will not be seekable.
+ * Only MSE compatible WebM files allow seekability for now.
+ */
+declare class Jamy extends EventEmitter {
+	/**
+	 * True if the stream is seekable
+	 */
+	public readonly seekable: boolean;
+	/**
+	 * The output stream
+	 */
+	public readonly stream: TimedStream;
+	/**
+	 * The current time of the presentation, in seconds
+	 */
+	public readonly currentTime: number;
+	/**
+	 * True if Jamy is playing something
+	 */
+	public readonly playing: boolean;
+
+	constructor(options?: Jamy.PlaybackOptions);
+
+	/**
+	 * Plays a media
+	 * @param {MediaInfos} infos Describes the media source
+	 */
+	public play(infos: Jamy.MediaInfos): void;
+	/**
+	 * Stops the playback and kill transcoder
+	 */
+	public stop(): void;
+	/**
+	 * Pauses the playback
+	 */
+	public pause(): void;
+	/**
+	 * Resumes the playback
+	 */
+	public resume(): void;
+	/**
+	 * Seeks the given time in seconds
+	 * If the media is not seekable, does nothing but emit and error
+	 * @param {number} time Timecode in seconds. If negative, timecode will be substracted from the end
+	 */
+	public seek(time: number): void;
+
+}
+
+/*~ If you want to expose types from your module as well, you can
+ *~ place them in this block.
+ */
+declare namespace Jamy {
+	export interface PlaybackOptions {
+		bitDepth: number,
+		frequency: number,
+		format: string,
+		channels: number
+	}
+	export interface MediaInfos {
+		url?: string,
+		index?: string | Range,
+		init?: string | Range,
+		type?: string,
+		stream?: Readable
+	}
+}
