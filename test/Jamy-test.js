81707cc419d95af08cf2374500942d038ef804a2
TinyMan <raph.a.uzan@gmail.com>
2017-06-20 15:27:28 +0200

    Test coverage for MediaProvider and some of Jamy

--- test/Jamy-test.js
+++ test/Jamy-test.js
@@ -1,15 +1,75 @@
 const chai = require('chai');
 const expect = chai.expect;
+const { PassThrough } = require('stream')
 
 const Jamy = require('../');
 
 describe('Jamy', () => {
+	let defaultJamy
+	beforeEach(() => {
+		defaultJamy = new Jamy()
+	});
 	it('should instanciate', done => {
 		const options = { format: "s16le", channels: 1, frequency: 4800, bitDepth: 16 };
 		const j = new Jamy(options);
 		expect(j).to.have.property('playbackOptions')
 		expect(j.playbackOptions).to.deep.equal(options)
+		expect(j.stream).to.be.instanceOf(PassThrough)
 		done()
+	})
+	it("should not be seekable with only a url or stream provided", done => {
+		defaultJamy.play({
+			url: "http://127.0.0.1/"
+		})
+		expect(defaultJamy.seekable).to.be.false
+		defaultJamy.play({
+			stream: new PassThrough()
+		})
+		expect(defaultJamy.seekable).to.be.false
+		done()
+	})
+	it("should throw an error if no valid input is specified", done => {
+		expect(defaultJamy.play.bind(defaultJamy)).to.throw("Invalid input")
+		done()
+	})
+	describe(".currentTime", () => {
+		it("should be 0 when no media is playing", done => {
+			expect(defaultJamy.currentTime).to.be.equal(0)
+			done()
+		})
+	})
+	describe(".playing", () => {
+		it("should be false when Jamy is not playing anything", done => {
+			expect(defaultJamy.playing).to.be.false
+			done()
+		})
+		it("should be false when Jamy is playing something", done => {
+			defaultJamy.play({ url: "http://127.0.0.1/" })
+			expect(defaultJamy.playing).to.be.true
+			done()
+		})
+	})
+	describe("events", () => {
+		it("should emit play event", done => {
+			defaultJamy.on('play', () => {
+				expect(defaultJamy.playing).to.be.true
+				done()
+			})
+			defaultJamy.play({ url: "http://127.0.0.1" })
+		})
+		it("should emit naturalEnd event", done => {
+			defaultJamy.on('naturalEnd', () => {
+				expect(defaultJamy.playing).to.be.false
+				done()
+			})
+			defaultJamy.play({ url: "http://127.0.0.1" })
+		})
+	})
+	describe("seek", () => {
+		it("should throw an error if stream is not seekable", done => {
+			expect(defaultJamy.seek.bind(defaultJamy)).to.throw("Media is not seekable")
+			done()
+		})
 	})
 })
 
--- test/MediaProvider-test.js
+++ test/MediaProvider-test.js
@@ -1,19 +1,15 @@
 const chai = require('chai')
-const nock = require('nock')
 const rewire = require('rewire');
 const expect = chai.expect;
+const mockery = require('mockery')
+const fs = require('fs')
 
 const MediaProvider = rewire('../lib/MediaProvider.js')
 
 
 const mochaAsync = (fn) => {
-	return async (done) => {
-		try {
-			await fn();
-			done();
-		} catch (err) {
-			done(err);
-		}
+	return done => {
+		fn().then(() => done()).catch(done)
 	};
 };
 
@@ -35,13 +31,45 @@ describe('MediaProvider', () => {
 		expect(m.ranges.index).to.be.deep.equal({ start: 201, end: 500 })
 		done()
 	})
-	it("should throw an error if specified range is invalid", done => {
+	it("should throw an error if specified range is invalid", mochaAsync(async () => {
 		const m = new MediaProvider()
-		m.getRange(4, 2)
-			.then(() => done(new Error("getRange should have thrown an error")))
-			.catch(e => {
-				if (e.message !== "Invalid range") done(new Error("Wrong error " + e))
-				else done()
+		try {
+			await m.getRange(4, 2)
+			throw new Error("getRange should have thrown an error")
+		} catch (e) {
+			if (e.message !== "Invalid range") throw new Error("Wrong error " + e)
+		}
+	}))
+	describe("mocked queries", () => {
+		const file = fs.readFileSync(__dirname + "/samples/DTMF2000ms-vorbis.webm")
+		before(() => {
+			mockery.enable({
+				warnOnReplace: false,
+				warnOnUnregistered: false,
+				useCleanCache: true
+			});
+			mockery.registerMock('request-promise-native', function (e) {
+				const range = parseRange(e.headers.Range.split('=')[1])
+				const bytes = file.slice(range.start, range.end)
+				return Promise.resolve(bytes)
 			})
+		})
+		after(() => {
+			mockery.disable();
+			mockery.deregisterAll();
+		})
+		it("should return the correct portions of the file", mochaAsync(async () => {
+			const m = new (require('../lib/MediaProvider.js'))({
+				url: "http://127.0.0.1/ok",
+				init: "0-100",
+				index: "101-200"
+			})
+			let bytes = await m.getInit()
+			expect(bytes.length).to.be.equal(100)
+			expect(bytes.compare(file, 0, 100)).to.be.equal(0)
+			bytes = await m.getIndex()
+			expect(bytes.length).to.be.equal(99)
+			expect(bytes.compare(file, 101, 200)).to.be.equal(0)
+		}))
 	})
 })
